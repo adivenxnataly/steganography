2b9fafa241f860c5989968cc070f2970192a1e01
Adinata <adinata.ch@proton.me>
2024-12-29 21:31:15 +0700

    program files

--- src/Sf.java
+++ src/Sf.java
@@ -2,7 +2,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import java.util.Scanner;
+import java.util.*;
 
 public class Sf {
     public static void main(String[] args) {
